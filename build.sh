b001815eb0193e3920b483e1ace33ecdd537c1aa
qinhuaiqin <150934137+qinhuaiqin@users.noreply.github.com>
2025-01-04 22:46:06 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -e
+
+# Check if 'builds' folder exists, create it if not
+if [ ! -d "./builds" ]; then
+    echo "'builds' folder not found. Creating it..."
+    mkdir -p ./builds
+else
+    echo "'builds' folder already exists removing it."
+    rm -rf ./builds
+    mkdir -p ./builds
+fi
+
+# Create the root folder with the current date and time (AM/PM)
+cd ./builds
+ROOT_DIR="OP12-A15-$(date +'%Y-%m-%d-%I-%M-%p')-release"
+echo "Creating root folder $ROOT_DIR..."
+mkdir -p "$ROOT_DIR"
+cd "$ROOT_DIR"
+
+# Clone the repositories into the root folder
+echo "Cloning repositories..."
+git clone https://github.com/TheWildJames/AnyKernel3.git -b android14-5.15
+git clone https://gitlab.com/simonpunk/susfs4ksu.git -b gki-android14-6.1
+git clone https://github.com/TheWildJames/kernel_patches.git
+
+# Get the kernel
+echo "Get the kernel..."
+mkdir oneplus12_v
+cd ./oneplus12_v
+repo init -u https://github.com/OnePlusOSS/kernel_manifest.git -b oneplus/sm8650 -m oneplus12_v.xml
+repo sync -j$(nproc)
+
+rm -rf ./kernel_platform/common/android/abi_gki_protected_exports_*
+
+# Add KernelSU
+echo "adding ksu"
+cd ./kernel_platform
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU/next/kernel/setup.sh" | bash -s next
+cd ./KernelSU-Next/kernel
+sed -i 's/ccflags-y += -DKSU_VERSION=16/ccflags-y += -DKSU_VERSION=12113/' ./Makefile
+cd ../../
+
+#add susfs
+echo "adding susfs"
+cp ../../susfs4ksu/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch ./KernelSU-Next/
+cp ../../susfs4ksu/kernel_patches/50_add_susfs_in_gki-android14-6.1.patch ./common/
+cp ../../susfs4ksu/kernel_patches/fs/susfs.c ./common/fs/
+cp ../../susfs4ksu/kernel_patches/include/linux/susfs.h ./common/include/linux/
+cd ./KernelSU-Next/
+patch -p1 < 10_enable_susfs_for_ksu.patch
+cd ../common
+patch -p1 < 50_add_susfs_in_gki-android14-6.1.patch || true
+cp ../../kernel_patches/69_hide_stuff.patch ./
+patch -p1 -F 3 < 69_hide_stuff.patch || true
+sed -i '/obj-\$(CONFIG_KSU_SUSFS_SUS_SU) += sus_su.o/d' ./fs/Makefile
+cd ..
+cp ../kernel_patches/selinux.c_fix.patch ./
+patch -p1 -F 3 < selinux.c_fix.patch
+cp ../kernel_patches/apk_sign.c_fix.patch ./
+patch -p1 -F 3 < apk_sign.c_fix.patch
+cp ../kernel_patches/Makefile_fix.patch ./
+patch -p1 --fuzz=3 < ./Makefile_fix.patch
+
+#build Kernel
+echo "CONFIG_KSU=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS=y" >> ./common/arch/arm64/configs/gki_defconfig
+echo "CONFIG_KSU_SUSFS_SUS_SU=y" >> ./common/arch/arm64/configs/gki_defconfig
+cd ..
+sed -i '2s/check_defconfig//' ./kernel_platform/common/build.config.gki
+./kernel_platform/oplus/build/oplus_build_kernel.sh pineapple gki
+
+# Copy Image.lz4
+echo "Copying Image"
+cp ./out/dist/Image ../AnyKernel3/Image
+
+# Navigate to the AnyKernel3 directory
+echo "Navigating to AnyKernel3 directory..."
+cd ../AnyKernel3
+
+# Zip the files in the AnyKernel3 directory with a new naming convention
+ZIP_NAME="Anykernel3-OP-A15-android14-6.1-KernelSU-SUSFS-$(date +'%Y-%m-%d-%H-%M-%S').zip"
+echo "Creating zip file $ZIP_NAME..."
+zip -r "../$ZIP_NAME" ./*
+
+# Move back to the root directory (assuming you are already in the correct directory)
+cd ..
